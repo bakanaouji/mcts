483452c7f56d3a05fb613e9e3488a61e1356f3c4
bakanaouji <abe@ic.dis.titech.ac.jp>
2018-05-21 02:02:43 +0900

    モンテカルロ木探索実装中

--- gametree.js
+++ gametree.js
@@ -8,3 +8,23 @@ function makeGameTree(board, player, wasPassed) {
         moves: possibleMoveList(board, player, wasPassed)
     };
 }
+
+/**
+ * N手先までのゲーム木を構築する
+ */
+function limitGameTreeDepth(gameTree, depth) {
+    return {
+        board: gameTree.board,
+        player: gameTree.player,
+        moves: depth == 0 ? [] : gameTree.moves.map(function (m) {
+            return {
+                isPassingMove: m.isPassingMove,
+                x: m.x,
+                y: m.y,
+                gameTreePromise: delay(function() {
+                    return limitGameTreeDepth(force(m.gameTreePromise), depth - 1);
+                })
+            };
+        })
+    };
+}
