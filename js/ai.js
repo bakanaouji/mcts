04470d6e3ce08dee5881536b01fe879a4187b85f
bakanaouji <abeken1224@yahoo.co.jp>
2018-05-26 00:25:14 +0900

    add comment

--- js/ai.js
+++ js/ai.js
@@ -50,22 +50,32 @@ function makeMonteCarloTreeSearchBasedAI(options) {
   };
 }
 
+/**
+ * モンテカルロ木探索によって行動を選択
+ */
 function tryMonteCarloTreeSearch(rootGameTree, maxTries) {
+  // 根ノード
   var root = new Node(rootGameTree, null, null);
 
+  // 一定回数分，プレイアウトを実行
   for (var i = 0; i < maxTries; i++) {
     var node = root;
 
+    // 展開していない行動があるか，
+    // 葉ノードに到達するまで，ノードをたどっていく
     while (node.untriedMoves.length === 0 && node.childNodes.length !== 0) {
       node = node.selectChild();
     }
 
+    // 展開していない行動がある場合，ノードを展開する
     if (node.untriedMoves.length !== 0) {
       node = node.expandChild();
     }
 
+    // ランダムにプレイアウトを実行
     var won = node.simulate(rootGameTree.player);
 
+    // back propagationで各ノードを評価
     node.backpropagate(won);
   }
 
@@ -73,6 +83,9 @@ function tryMonteCarloTreeSearch(rootGameTree, maxTries) {
   return root.childNodes[vs.indexOf(Math.max.apply(null, vs))].move;
 }
 
+/**
+ * ノードを表すクラス
+ */
 function Node(gameTree, parentNode, move) {
   this.gameTree = gameTree;
   this.parentNode = parentNode;
@@ -83,6 +96,9 @@ function Node(gameTree, parentNode, move) {
   this.untriedMoves = gameTree.moves.slice();
 }
 
+/**
+ * UCB戦略に従ってノードを選択
+ */
 Node.prototype.selectChild = function () {
   var totalVisits = this.visits;
   var values = this.childNodes.map(function (n) {
@@ -92,29 +108,45 @@ Node.prototype.selectChild = function () {
   return this.childNodes[values.indexOf(Math.max.apply(null, values))];
 };
 
+/**
+ * ノードを展開
+ */
 Node.prototype.expandChild = function () {
+  // 展開していない行動のなかからランダムに選択
   var i = random(this.untriedMoves.length);
   var move = this.untriedMoves.splice(i, 1)[0];
+  // ノードを生成して子ノードとして追加
   var child = new Node(force(move.gameTreePromise), this, move);
   this.childNodes.push(child);
   return child;
 };
 
+/**
+ * プレイアウト
+ */
 Node.prototype.simulate = function (player) {
   var gameTree = this.gameTree;
+  // ランダムに行動を選択してゲーム終了まで進める
   while (gameTree.moves.length !== 0) {
     var i = random(gameTree.moves.length);
     gameTree = force(gameTree.moves[i].gameTreePromise);
   }
+  // 勝敗を評価
   return judge(gameTree.board) * (player === BLACK ? 1 : -1) / 2 + 0.5;
 };
 
+/**
+ * back propagationで各ノードを評価
+ */
 Node.prototype.backpropagate = function (result) {
   for (var node = this; node !== null; node = node.parentNode) {
     node.update(result);
   }
 };
 
+/**
+ * ノードの評価を更新
+ */
 Node.prototype.update = function (won) {
   this.wins += won;
   this.visits += 1;
